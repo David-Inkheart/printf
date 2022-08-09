4cc1c39ae92cc757a2003558496fccba0dfa1b5f
David-Inkheart <okoliedave1@gmail.com>
2022-08-09 08:02:10 -0700

    select formart specifier

--- main.h
+++ main.h
@@ -1,14 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdio.h>
+
 int _printf(const char *format, ...);
 
  typedef struct fmsp
     {
-        char flag;
+        char *flag;
         char *(*fg)(va_list ap);
     }fmsp;
 char *char_print(va_list ap);
 char *percent_print(va_list ap);
 char *str_print(va_list ap);
+char* (*get_fmsp_func(char *s))(va_list ap);
+
 #endif /* MAIN_H */
